0839406c5af5043076025228b0eded77557fc7cc
personal <iemfectum@gmail.com>
2022-05-01 19:33:02 +0530

    Pair Random Problem solved

--- Leetcode/index.js
+++ Leetcode/index.js
@@ -14,30 +14,30 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
- var intersect = function(nums1, nums2) {
-    let arr = [];
-    for (let i = 0; i < nums1.length; i++) {
-      const element = nums1[i];
-      let index = nums2.indexOf(element)
-      if( index !== -1){
-        arr = arr.concat(nums2.splice(index, 1))
-      }
-    }
-    console.log(arr)
-    return arr;
-};
+//  var intersect = function(nums1, nums2) {
+//     let arr = [];
+//     for (let i = 0; i < nums1.length; i++) {
+//       const element = nums1[i];
+//       let index = nums2.indexOf(element)
+//       if( index !== -1){
+//         arr = arr.concat(nums2.splice(index, 1))
+//       }
+//     }
+//     console.log(arr)
+//     return arr;
+// };
 
-const crop = (message, maxLength) => {
-  if(maxLength > message.length){
-    return message
-  }
-  const part = message.substring(0, maxLength + 1);
-  return part.substring(0, part.lastIndexOf(" ")).trimEnd();
-}
+// const crop = (message, maxLength) => {
+//   if(maxLength > message.length){
+//     return message
+//   }
+//   const part = message.substring(0, maxLength + 1);
+//   return part.substring(0, part.lastIndexOf(" ")).trimEnd();
+// }
 
-console.log(crop("Codality we test coder", 14));
-console.log(crop("Why not", 14));
-Ques:
+// console.log(crop("Codality we test coder", 14));
+// console.log(crop("Why not", 14));
+// Ques:
 // Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length 
 // and answer[i] is the distance from index i to the closest occurrence of character c in s.
 
@@ -52,4 +52,30 @@ Ques:
 // 1. pankaj === [3,2,1,0,1,2]
 
 // intersect([1,2,2,1],[2,2])    
-// intersect([4,9,5],[9,4,9,8,4])    
+// intersect([4,9,5],[9,4,9,8,4])   
+
+
+// pair Random
+
+// input = ["Amar","Rohit","Arjith","Aayush","Dipti","Prafful"]
+// output = [["Amar","Rohit"],["Arjith","Prafull"],["Dipti",null]] 
+
+Array.prototype.random = function () {
+  let index = Math.floor((Math.random() * this.length))
+  let value = this[index]
+  return [value, index]
+  
+}
+
+function pairRandom(input){
+  let output = []
+  while(input.length){
+    let person1 = input.shift()
+    let [person2, index] = input.random()
+    input.splice(index, 1)
+    output.push([person1, person2])
+  }   
+  return output
+}~
+console.log(pairRandom(["Amar","Rohit","Arjith","Aayush","Dipti","Prafful","Sashi"]))
+// console.log(pairRandom(["Amar"]))
